dd330292f39aa43117c0039c2eebd117391e3d42
Maher Tabbaa <mahertba999@gmail.com>
2020-06-20 22:20:04 +0300

    assign json pages to user

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -123,7 +123,8 @@ class GraphController extends Controller
             //todo get name of publish name index
 //            $share->page_name = ['id'=>$request->page_id, 'name'=>'page name'];
             //$share->page_name = Auth::user()->user_pages->get(['id'=>$request->page_id]);
-            dd($request->page_id[0]->toArray());
+            $collection = collect($request->page_id);
+            dd($collection->whereIn('id', $request->page_id));
             $share->page_name = Auth::user()->pages->whereIn('id', $request->page_id);
             $share->data =  $request->message;
             //todo create function to attach image
